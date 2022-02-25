0ed1be416868249fd8d680e77d61497d56e9fde9
Jon Clucas <jon.clucas@childmind.org>
2022-02-25 10:39:03 -0500

    :bulb: Fix expected input tree
    [skip ci]

--- .github/scripts/minify-freesurfer-6.0.1.sh
+++ .github/scripts/minify-freesurfer-6.0.1.sh
@@ -13,27 +13,10 @@
 #             ├── ds000239-sub-01_run-02
 #             │   └── sub-01_run-02_T1w.nii.gz  # s3://openneuro.org/ds000239/sub-01/anat/sub-01_run-02_T1w.nii.gz
 #             └── studyforrest-sub-01
-#                 ├── README.md
-#                 └── original
-#                     └── 3T_structural_mri
-#                         ├── CHANGES
-#                         ├── ISSUES
-#                         ├── LICENSE
-#                         ├── README.md
-#                         ├── datacite.yml
-#                         ├── datalad_description.json
-#                         └── sub-01
-#                             └── anat
-#                                 ├── sub-01_SWI.json
-#                                 ├── sub-01_SWI_defacemask.nii.gz
-#                                 ├── sub-01_SWImag.nii.gz
-#                                 ├── sub-01_SWIphase.nii.gz
-#                                 ├── sub-01_T1w.json
-#                                 ├── sub-01_T1w.nii.gz
-#                                 ├── sub-01_T1w_defacemask.nii.gz
-#                                 ├── sub-01_T2w.json
-#                                 ├── sub-01_T2w.nii.gz
-#                                 └── sub-01_T2w_defacemask.nii.gz
+#                 ├── sub-01_T1w.json
+#                 ├── sub-01_T1w.nii.gz
+#                 ├── sub-01_T2w.json
+#                 └── sub-01_T2w.nii.gz
 
 # Set up local env vars
 DATA_DIR=${PWD}/data/fs601
